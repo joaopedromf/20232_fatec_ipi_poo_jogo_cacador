bf9d785555bd9244348db826bc83e2d75a1b67aa
João Pedro Martins Forte <joao.forte@fatec.sp.gov.br>
2023-09-28 02:04:02 -0300

    ap07(poo-p2): encapsulamento e baixo acoplamento

--- Personagem.java
+++ Personagem.java
@@ -1,9 +1,28 @@
 public class Personagem{
     //atributos
-    String nome;
-    int energia = 10;
-    int fome = 0;
-    int sono = 0;
+    private String nome;
+    private int energia = 10;
+    private int fome = 0;
+    private int sono = 0;
+
+    // construtor: lista de parâmetros vazia
+    // Personagem(){
+
+    // }
+
+    Personagem(String nome){
+        this.nome = nome;
+    }
+
+    Personagem(String nome,int energia,int fome,int sono){
+        this.nome = nome;
+        if (energia >= 0 && energia <= 10)
+            this.energia = energia;
+        if (fome >= 0 && fome <= 10)
+            this.fome = fome;
+        if(sono >= 0 && sono <= 10)
+            this.sono = sono; 
+    }
 
     //métodos
     void cacar(){
@@ -38,5 +57,15 @@ public class Personagem{
         else{
             System.out.println(nome + " sem sono.");
         }
+    }
+
+    void exibirEstado(){
+        System.out.printf(
+            "%s: e: %d, f: %d, s: %d\n",
+            nome,
+            energia,
+            fome,
+            sono
+        );
     }
 }
